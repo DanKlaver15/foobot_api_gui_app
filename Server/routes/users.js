de051dda92e2e4e7220ac9b85dc1216749a56525
DKlaver <dklaver15@gmail.com>
2021-04-10 14:27:04 -0500

    Added routes to handle avatars.

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -4,5 +4,23 @@ const Joi = require("joi");
 const userController = require("../controllers/userController");
 const auth = require("../middlewares/auth");
 const { validateUser } = require("../middlewares/validate");
+const avatar = require("../middlewares/avatar");
 
 router.route("/").post(validateUser, userController.createOne);
+router
+  .route("/:id")
+  .get(auth, userController.getOne)
+  .put(auth, userController.updateOne)
+  .delete(auth, userController.removeOne);
+
+router.get("/avatar/:filename", (req, res) => {
+  res.type("png");
+  return res.sendFile(avatar.filepath(req.params.filename));
+});
+
+router
+  .route("/:id/avatar")
+  .post([auth, avatar.upload, avatar.handleAvatar()], userController.addAvatar)
+  .delete(auth, userController.removeAvatar);
+
+module.exports = router;
