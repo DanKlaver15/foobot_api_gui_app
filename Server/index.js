6d070ef780eeb84027869d749480ee9b5fbb18a8
DKlaver <dklaver15@gmail.com>
2021-04-10 12:12:59 -0500

    Initial commit.

---
+++ Server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
