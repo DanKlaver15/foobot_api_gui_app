9dc4b8e2e904383351b2e3b322b5a66cba500782
DKlaver <dklaver15@gmail.com>
2021-04-10 14:39:41 -0500

    Fixed import path for device controller.

--- Server/routes/devices.js
+++ Server/routes/devices.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const deviceController = require("../deviceController");
+const deviceController = require("../controllers/deviceController");
 const auth = require("../middlewares/auth");
 
 router
