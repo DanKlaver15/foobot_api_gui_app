ef1304229b6b5ee9ea919ad392eee461ff47d769
DKlaver <dklaver15@gmail.com>
2021-04-10 14:28:04 -0500

    Added required properties to the schema.

--- Server/models/device.js
+++ Server/models/device.js
@@ -3,13 +3,17 @@ const mongoose = require("mongoose");
 const deviceSchema = new mongoose.Schema({
   uuid: {
     type: String,
+    required: true,
   },
   mac: {
     type: String,
+    required: true,
   },
   name: {
     type: String,
+    required: true,
   },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
 const Device = mongoose.model("Device", deviceSchema);
