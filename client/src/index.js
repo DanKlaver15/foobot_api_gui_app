1bc7bb8c262518d8a086323bd2784e73428ccf63
DKlaver <dklaver15@gmail.com>
2021-04-10 17:34:14 -0500

    Added router to index for different pages.

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./state/store";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={configureStore()}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
