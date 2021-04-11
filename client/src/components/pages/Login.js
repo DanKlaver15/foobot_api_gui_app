539d2698538bae8baffa62d09acced2716ce4b30
DKlaver <dklaver15@gmail.com>
2021-04-10 21:24:04 -0500

    Removed the navigation component as it is no longer needed.

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,11 +1,9 @@
 import React from "react";
 import LoginForm from "../forms/LoginForm";
-import Navigation from "../Navigation";
 
 const Login = () => {
   return (
     <div className="bg-gray-50 min-h-screen">
-      <Navigation />
       <LoginForm />
     </div>
   );
--- client/src/components/pages/Registration.js
+++ client/src/components/pages/Registration.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Navigation from "../Navigation";
 import RegistrationForm from "../forms/RegistrationForm";
 
 const Registration = () => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navigation />
       <RegistrationForm />
     </div>
   );
