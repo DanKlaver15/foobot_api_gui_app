34bddd325c6a97004e023cf947feceafbdf0d146
DKlaver <dklaver15@gmail.com>
2021-04-12 21:21:02 -0500

    Updated login form to actually sent API call to login.

--- client/src/components/forms/LoginForm.js
+++ client/src/components/forms/LoginForm.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { loginRequest } from "../../state/User/thunks";
 import Logo from "../../logo/Logo";
 
 function LoginForm({ loginRequest, loginError }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const { pathname } = useLocation();
-  console.log(pathname);
 
   return (
     <div className="bg-white flex">
@@ -34,7 +31,14 @@ function LoginForm({ loginRequest, loginError }) {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="mt-6">
-            <form action="#" method="POST" className="space-y-6">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                loginRequest({ email, password });
+              }}
+              method="POST"
+              className="space-y-6"
+            >
               <div>
                 <label
                   htmlFor="email"
@@ -44,12 +48,14 @@ function LoginForm({ loginRequest, loginError }) {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={(e) => setEmail(e.target.value)}
                     id="email"
                     name="email"
                     type="email"
                     autoComplete="email"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    value={email}
                   />
                 </div>
               </div>
@@ -63,38 +69,44 @@ function LoginForm({ loginRequest, loginError }) {
                 </label>
                 <div className="mt-1">
                   <input
+                    onChange={(e) => setPassword(e.target.value)}
                     id="password"
                     name="password"
                     type="password"
                     autoComplete="current-password"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    value={password}
                   />
                 </div>
               </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember_me"
-                    name="remember_me"
-                    type="checkbox"
-                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                  />
-                  <label
-                    htmlFor="remember_me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    Remember me
-                  </label>
+              {loginError && (
+                <div className="rounded-md bg-red-50 p-4">
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <svg
+                        className="h-5 w-5 text-red-400"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </div>
+                    <div className="ml-3">
+                      <h3 className="text-sm font-medium text-red-800">
+                        {loginError}
+                      </h3>
+                    </div>
+                  </div>
                 </div>
-
-                <div className="text-sm">
-                  <button className="font-medium text-indigo-600 hover:text-indigo-500">
-                    Forgot your password?
-                  </button>
-                </div>
-              </div>
+              )}
 
               <div>
                 <button
