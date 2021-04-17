a1521adfff24f57887a8a6aa9c3c2d96e2d55b75
DKlaver <dklaver15@gmail.com>
2021-04-17 10:07:16 -0500

    Updated logo with name.

--- client/src/logo/LogoNameDark.js
+++ client/src/logo/LogoNameDark.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./botadora_logo_name_black.png.png";
+import logo from "./logo_alt.svg";
 
 const LogoNameDark = ({ size = "auto" }) => {
   return <img className={`h-auto w-${size}`} src={logo} alt={"logo"} />;
--- client/src/logo/LogoNameLight.js
+++ client/src/logo/LogoNameLight.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./botadora_logo_name_white.png";
+import logo from "./logo_alt.svg";
 
 const LogoNameLight = ({ size = "auto" }) => {
   return <img className={`h-auto w-${size}`} src={logo} alt={"logo"} />;
