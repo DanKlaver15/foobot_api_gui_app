84714026740305ae0172d028e9f88e3ade1b8193
DKlaver <dklaver15@gmail.com>
2021-04-20 17:24:19 -0500

    Replaced avatar with a card containing a welcome back message for the user.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import Avatar from "../Avatar";
+import WelcomeCard from "../WelcomeCard";
 
-const Home = ({ user }) => {
+const Home = () => {
   return (
     <>
       <div className="flex space-x-3">
-        <div className="flex-shrink-0">
-          <Avatar size={10} source={user.avatar} />
-        </div>
+        <WelcomeCard />
         <div className="min-w-0 flex-1" />
       </div>
     </>
