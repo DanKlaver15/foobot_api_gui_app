3f570881aa84482e8d6b2d7419c16bdd837132ea
DKlaver <dklaver15@gmail.com>
2021-04-21 21:19:28 -0500

    Removed unneeded sections.

--- client/src/components/DataDownload.js
+++ client/src/components/DataDownload.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import DataFormatSetting from "./RadioGroup";
 import { connect } from "react-redux";
 import DateFnsUtils from "@date-io/date-fns"; // choose your lib
 import {
@@ -50,6 +51,7 @@ const DataDownload = () => {
 
   const [startDate, setStartDate] = useState(yesterday);
   const [endDate, setEndDate] = useState(new Date());
+  const [sensorList, setSensorList] = useState("pm,voc,hum,co2,tmp,allpollu");
 
   return (
     <div>
@@ -69,8 +71,8 @@ const DataDownload = () => {
           </div>
           <div className="mt-5 md:mt-0 md:col-span-2">
             <form action="#" method="POST">
-              <div className="grid grid-cols-6 gap-6">
-                <div className="col-span-6 sm:col-span-4">
+              <div className="grid grid-cols-7 gap-8">
+                <div className="col-span-4">
                   <label
                     htmlFor="uuid"
                     className="block text-sm font-medium text-gray-700 dark:text-gray-400"
@@ -79,14 +81,14 @@ const DataDownload = () => {
                   </label>
                   <input
                     type="text"
-                    name="first_name"
-                    id="first_name"
-                    autoComplete="given-name"
-                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    disabled="true"
+                    name="uuid"
+                    id="uuid"
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md dark:bg-gray-400"
                   />
                 </div>
 
-                <div className="col-span-6 sm:col-span-3">
+                <div className="col-span-2 sm:col-span-1.5 row-start-2">
                   <label
                     htmlFor="last_name"
                     className="block text-sm font-medium text-gray-700 dark:text-gray-400"
@@ -109,7 +111,7 @@ const DataDownload = () => {
                   </ThemeProvider>
                 </div>
 
-                <div className="col-span-6 sm:col-span-3">
+                <div className="col-span-2 sm:col-span-1.5 row-start-2">
                   <label
                     htmlFor="email_address"
                     className="block text-sm font-medium text-gray-700 dark:text-gray-400"
@@ -120,10 +122,10 @@ const DataDownload = () => {
                     <MuiPickersUtilsProvider utils={DateFnsUtils}>
                       <KeyboardDatePicker
                         clearable
+                        disableFuture="true"
                         variant="inline"
                         value={endDate}
                         onChange={(date) => setEndDate(date)}
-                        minDate={new Date()}
                         format="MM/dd/yyyy"
                         placeholder="Pick an end date"
                         id="end_date"
@@ -133,229 +135,41 @@ const DataDownload = () => {
                   </ThemeProvider>
                 </div>
 
-                <div className="col-span-6 sm:col-span-3">
+                <div className="col-span-4 row-start-3">
                   <label
-                    htmlFor="country"
-                    className="block text-sm font-medium text-gray-700"
+                    htmlFor="average_by"
+                    className="block text-sm font-medium text-gray-700 dark:text-gray-400"
                   >
-                    Country / Region
+                    Average By
                   </label>
-                  <select
-                    id="country"
-                    name="country"
-                    autoComplete="country"
-                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
-                  </select>
+                  <input
+                    type="text"
+                    name="average_by"
+                    id="average_by"
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md dark:bg-gray-400"
+                  />
                 </div>
 
-                <div className="col-span-6">
+                <div className="col-span-4 row-start-4">
                   <label
                     htmlFor="street_address"
-                    className="block text-sm font-medium text-gray-700"
+                    className="block text-sm font-medium text-gray-700 dark:text-gray-400"
                   >
-                    Street address
+                    Sensor List
                   </label>
                   <input
+                    value={sensorList}
                     type="text"
-                    name="street_address"
-                    id="street_address"
-                    autoComplete="street-address"
-                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    name="sensor_list"
+                    id="sensor_list"
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md dark:bg-gray-400"
                   />
                 </div>
 
-                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                  <label
-                    htmlFor="city"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    City
-                  </label>
-                  <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  />
-                </div>
-
-                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                  <label
-                    htmlFor="state"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    State / Province
-                  </label>
-                  <input
-                    type="text"
-                    name="state"
-                    id="state"
-                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  />
-                </div>
-
-                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                  <label
-                    htmlFor="postal_code"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    ZIP / Postal
-                  </label>
-                  <input
-                    type="text"
-                    name="postal_code"
-                    id="postal_code"
-                    autoComplete="postal-code"
-                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  />
+                <div className="col-span-4 row-start-5 row-span-1">
+                  <DataFormatSetting />
                 </div>
               </div>
-            </form>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-white dark:bg-gray-600 shadow px-4 py-5 sm:rounded-lg sm:p-6">
-        <div className="md:grid md:grid-cols-3 md:gap-6">
-          <div className="md:col-span-1">
-            <h3 className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-300">
-              Notifications
-            </h3>
-            <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
-              Decide which communications you'd like to receive and how.
-            </p>
-          </div>
-          <div className="mt-5 md:mt-0 md:col-span-2">
-            <form className="space-y-6" action="#" method="POST">
-              <fieldset>
-                <legend className="text-base font-medium text-gray-900">
-                  By Email
-                </legend>
-                <div className="mt-4 space-y-4">
-                  <div className="flex items-start">
-                    <div className="h-5 flex items-center">
-                      <input
-                        id="comments"
-                        name="comments"
-                        type="checkbox"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        htmlFor="comments"
-                        className="font-medium text-gray-700"
-                      >
-                        Comments
-                      </label>
-                      <p className="text-gray-500">
-                        Get notified when someones posts a comment on a posting.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="candidates"
-                        name="candidates"
-                        type="checkbox"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        htmlFor="candidates"
-                        className="font-medium text-gray-700"
-                      >
-                        Candidates
-                      </label>
-                      <p className="text-gray-500">
-                        Get notified when a candidate applies for a job.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="offers"
-                        name="offers"
-                        type="checkbox"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        htmlFor="offers"
-                        className="font-medium text-gray-700"
-                      >
-                        Offers
-                      </label>
-                      <p className="text-gray-500">
-                        Get notified when a candidate accepts or rejects an
-                        offer.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </fieldset>
-              <fieldset>
-                <div>
-                  <legend className="text-base font-medium text-gray-900">
-                    Push Notifications
-                  </legend>
-                  <p className="text-sm text-gray-500">
-                    These are delivered via SMS to your mobile phone.
-                  </p>
-                </div>
-                <div className="mt-4 space-y-4">
-                  <div className="flex items-center">
-                    <input
-                      id="push_everything"
-                      name="push_notifications"
-                      type="radio"
-                      className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                    />
-                    <label
-                      htmlFor="push_everything"
-                      className="ml-3 block text-sm font-medium text-gray-700"
-                    >
-                      Everything
-                    </label>
-                  </div>
-                  <div className="flex items-center">
-                    <input
-                      id="push_email"
-                      name="push_notifications"
-                      type="radio"
-                      className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                    />
-                    <label
-                      htmlFor="push_email"
-                      className="ml-3 block text-sm font-medium text-gray-700"
-                    >
-                      Same as email
-                    </label>
-                  </div>
-                  <div className="flex items-center">
-                    <input
-                      id="push_nothing"
-                      name="push_notifications"
-                      type="radio"
-                      className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                    />
-                    <label
-                      htmlFor="push_nothing"
-                      className="ml-3 block text-sm font-medium text-gray-700"
-                    >
-                      No push notifications
-                    </label>
-                  </div>
-                </div>
-              </fieldset>
             </form>
           </div>
         </div>
