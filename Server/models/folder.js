26e37147029337fce0dcf49a9ce9e498206582ce
DKlaver <dklaver15@gmail.com>
2021-04-27 19:41:51 -0500

    Renamed several properties to integrate with the folder tree.

--- Server/models/folder.js
+++ Server/models/folder.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 
 const folderSchema = new mongoose.Schema({
-  parent: {
-    type: mongoose.Schema.Types.ObjectId,
-  },
-  name: {
+  title: {
     type: String,
     required: true,
   },
+  key: { type: mongoose.Schema.Types.ObjectId },
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-  devices: [{ type: mongoose.Schema.Types.ObjectId, ref: "Device" }],
-  children: [{ type: mongoose.Schema.Types.ObjectId }],
+  children: [{ type: Object }],
 });
 
 const Folder = mongoose.model("Folder", folderSchema);
