b2013d7525ef6379c3bd69a718832bd9948ac6c3
DKlaver <dklaver15@gmail.com>
2021-05-01 15:25:25 -0500

    Added styling for dark and light mode.

--- client/src/components/pages/ManageFoobots.js
+++ client/src/components/pages/ManageFoobots.js
@@ -34,7 +34,7 @@ const ManageFoobots = ({
     <div className="flex-1 relative z-0 flex overflow-hidden">
       <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
         <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
-          <div className="h-full border-2 border-gray-200 rounded-lg overflow-y-auto">
+          <div className="h-full border-2 border-indigo-300 dark:border-gray-200 rounded-lg overflow-y-auto">
             <div className="bg-white dark:bg-gray-600 dark:text-gray-300 overflow-hidden shadow rounded-lg">
               <div className="px-4 py-5 sm:p-6 text-center">
                 No Foobot Selected
@@ -50,9 +50,9 @@ const ManageFoobots = ({
           </div>
         </div>
       </main>
-      <aside className="hidden relative xl:order-first xl:flex xl:flex-col flex-shrink-0 w-96 border-r border-gray-200 overscroll-y-auto">
+      <aside className="hidden relative xl:order-first xl:flex xl:flex-col flex-shrink-0 w-96 border-r dark:border-gray-200 overscroll-y-auto">
         <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
-          <div className="h-full border-2 border-gray-200 rounded-lg dark:text-gray-400">
+          <div className="h-full border-2 border-indigo-300 bg-white dark:bg-gray-600 dark:border-gray-200 rounded-lg dark:text-gray-400">
             <span className="relative flex">
               <button
                 onClick={(e) => {
@@ -63,7 +63,7 @@ const ManageFoobots = ({
                   getFolderList(userId);
                 }}
                 type="button"
-                className="relative flex align-bottom items-center px-5 mb-4 w-1/2 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded--tl-md text-white bg-indigo-600 dark:bg-gray-600 dark:border-4 dark:border-gray-300 dark:text-gray-300"
+                className="relative flex align-bottom items-center px-5 mb-4 w-1/2 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-tl-md text-white bg-indigo-600 border-indigo-300 dark:bg-gray-600 dark:border-4 dark:border-gray-300 dark:text-gray-300"
               >
                 <FontAwesomeIcon
                   icon={faFolderPlus}
@@ -84,7 +84,7 @@ const ManageFoobots = ({
                   }
                 }}
                 type="button"
-                className="relative flex align-bottom items-center px-4 mb-4 w-1/2 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-tr-md text-white bg-indigo-600 dark:bg-gray-600 dark:border-4 dark:border-gray-300 dark:text-gray-300"
+                className="relative flex align-bottom items-center px-4 mb-4 w-1/2 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-tr-md text-white bg-indigo-600 border-indigo-300 dark:bg-gray-600 dark:border-4 dark:border-gray-300 dark:text-gray-300"
               >
                 <FontAwesomeIcon
                   icon={faFolderTimes}
