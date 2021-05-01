0a0225ad22698e717f58356d5365cad45d4a752b
DKlaver <dklaver15@gmail.com>
2021-05-01 16:19:54 -0500

    Additional formatting changes.

--- client/src/components/folderTree/RenameModal.js
+++ client/src/components/folderTree/RenameModal.js
@@ -67,7 +67,7 @@ const RenameModal = ({ folder, updateFolder }) => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+              <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75 transition-opacity" />
             </Transition.Child>
 
             {/* This element is to trick the browser into centering the modal contents. */}
@@ -86,10 +86,7 @@ const RenameModal = ({ folder, updateFolder }) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <div
-                role="none"
-                className="inline-block align-bottom bg-white dark:bg-gray-500 dark:text-gray-300 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
-              >
+              <div className="inline-block align-bottom bg-gray-200 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
                 <form
                   id="modalForm"
                   onSubmit={(e) => {
