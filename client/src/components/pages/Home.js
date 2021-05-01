bae1db40cde9ba67b144fd018c4de482be3fcfed
DKlaver <dklaver15@gmail.com>
2021-05-01 15:25:13 -0500

    Added robot icons to background.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import WelcomeCard from "../WelcomeCard";
+import RobotIconDark from "../../RobotIcons/RobotIconDark";
+import RobotIconLight from "../../RobotIcons/RobotIconLight";
 
-const Home = () => {
+const Home = ({ user }) => {
   return (
-    <>
+    <div className="grid grid-rows-4 grid-cols-2 min-height-1/2">
       <div className="flex space-x-3">
         <WelcomeCard />
         <div className="min-w-0 flex-1" />
       </div>
-    </>
+      <div className="row-start-3 row-span-2 justify-self-center">
+        {user.darkMode ? (
+          <RobotIconDark size={96} />
+        ) : (
+          <RobotIconLight size={96} />
+        )}
+      </div>
+    </div>
   );
 };
 
