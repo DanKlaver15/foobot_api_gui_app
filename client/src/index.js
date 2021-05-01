4463141f90f5cc1d9e95255ed9fce8f840667541
DKlaver <dklaver15@gmail.com>
2021-05-01 14:09:43 -0500

    Added import and statement for Sentry issue tracking.

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,18 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "./state/store";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
+
+Sentry.init({
+  dsn:
+    "https://f321f408d7994b06a14c1a401616e851@o592139.ingest.sentry.io/5740545",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  tracesSampleRate: 0.8,
+});
 
 ReactDOM.render(
   <Provider store={configureStore()}>
