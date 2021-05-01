6779060740b4643a3a0b1bad8bb3c8178fbe928d
DKlaver <dklaver15@gmail.com>
2021-05-01 14:10:52 -0500

    Added additional custom config for box shadow.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       full: "100%",
     },
     boxShadow: {
+      "4xl": "0 20px 25px -5px rgba(200, 200, 200, 0.5)",
       "3xl": "0 40px 65px -20px rgba(0, 0, 0, 0.3)",
       inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
     },
