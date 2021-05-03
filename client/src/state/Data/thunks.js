3c20f131ef5114992e958963205b5a6cd3c65f14
DKlaver <dklaver15@gmail.com>
2021-05-02 19:01:37 -0500

    Created thunk to loop through each of the user’s devices and call the Foobot API to populate one large dataset to be downloaded.

--- client/src/state/Data/thunks.js
+++ client/src/state/Data/thunks.js
@@ -37,3 +37,61 @@ export const updateDataRequest = (
     dispatch(dataInFailure);
   }
 };
+
+export const updateDataFromAllDevicesRequest = (
+  user,
+  start,
+  end,
+  averageBy,
+  dataFormat
+) => async (dispatch, getState) => {
+  dispatch(dataInProgress);
+
+  const download = (data) => {
+    if (dataFormat === "text/csv") {
+      let link = document.createElement("a");
+      link.href = window.URL.createObjectURL(new Blob([data], { type: "txt" }));
+      link.download = "foobot_data.txt";
+      document.body.appendChild(link);
+      link.click();
+      setTimeout(function () {
+        window.URL.revokeObjectURL(link);
+      }, 200);
+    } else {
+      let link = document.createElement("a");
+      link.href = window.URL.createObjectURL(
+        new Blob([data], { type: "json" })
+      );
+      link.download = "foobot_data.json";
+      document.body.appendChild(link);
+      link.click();
+      setTimeout(function () {
+        window.URL.revokeObjectURL(link);
+      }, 200);
+    }
+  };
+
+  let allData = [];
+
+  try {
+    user.devices.forEach((device) => async () => {
+      const response = await axios.get(
+        `https://api.foobot.io/v2/device/${device.uuid}/datapoint/${start}/${end}/${averageBy}/`,
+        {
+          headers: {
+            "x-api-key-token": user.apiKey,
+            accept: `${dataFormat};charset=UTF-8`,
+          },
+        }
+      );
+      allData.push(response.data);
+    });
+
+    download(allData);
+
+    dispatch(dataInSuccess());
+  } catch (err) {
+    console.log("error:", err);
+    dispatch(dataInFailure);
+  }
+};
